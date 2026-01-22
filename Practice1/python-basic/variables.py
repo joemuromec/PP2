595580d0521b228463c755911e10ccb42beaca0e
joemuromec <joemuromec@gmail.com>
2026-01-22 20:09:54 +0500

    added info about variables

---
+++ Practice1/python-basic/variables.py
@@ -0,0 +1,25 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+s = 4       # s is of type int
+s = "Sally" # s is now of type str
+print(s)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+x = "John"
+# is the same as
+x = 'John'
+
+a = 4
+A = "Sally"
+#A will not overwrite a
