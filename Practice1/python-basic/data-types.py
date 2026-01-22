beb857766a1059ba577b2816ceb667b2353d05fa
joemuromec <joemuromec@gmail.com>
2026-01-22 20:23:35 +0500

    added examples for data type

---
+++ Practice1/python-basic/data-types.py
@@ -0,0 +1,42 @@
+x = 5
+print(type(x))
+
+x = "Hello World"
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = ["apple", "banana", "cherry"]
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = range(10)
+
+#display x:
+print(x)
+
+#convert to list to display the content of x:
+print(list(x))
+
+x = {"apple", "banana", "cherry"}
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = complex(1j)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
