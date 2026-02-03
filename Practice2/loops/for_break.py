f1cc38a9d035c98750a2e02f4ac9970ea59c9920
joemuromec <joemuromec@gmail.com>
2026-02-03 17:48:02 +0500

    added examples of break in for loop

--- Practice2/loops/for_break.py
+++ Practice2/loops/for_break.py
@@ -1,1 +1,22 @@
- 
+# Exit the loop when x is "banana":
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+
+# Exit the loop when x is "banana", but this time the break comes before the print:
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
+#If the loop breaks, the else block is not executed.
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
+
+
