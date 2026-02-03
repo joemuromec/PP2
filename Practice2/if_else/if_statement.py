c09c5d9d571778b03e5a3d9dcd5133c5f8ff9f40
joemuromec <joemuromec@gmail.com>
2026-02-03 17:02:29 +0500

    added if_statement file

--- Practice2/if_else/if_statement.py
+++ Practice2/if_else/if_statement.py
@@ -1,1 +1,25 @@
- 
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+  
+number = 15
+if number > 0:
+  print("The number is positive")
+
+# You can have multiple statements inside an if block. All statements must be indented at the same level.
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+  
+# Using a boolean variable:
+is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")
+  
+a = 33
+b = 200
+if b > a:
+    print("b is greater than a") # you will get an error without indentation
