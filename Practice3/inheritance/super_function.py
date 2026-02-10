953df671849aeee20006f9dd35bfd28258e2bf86
joemuromec <joemuromec@gmail.com>
2026-02-10 13:58:57 +0500

    modified super function file

--- Practice3/inheritance/super_function.py
+++ Practice3/inheritance/super_function.py
@@ -10,3 +10,11 @@ class Person:
 class Student(Person):
   def __init__(self, fname, lname):
     super().__init__(fname, lname)
+    
+# Add a year parameter, and pass the correct year when creating objects:
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+x = Student("Mike", "Olsen", 2019)
