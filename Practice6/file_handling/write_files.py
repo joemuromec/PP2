edb0e498a1d60b9e4145c5760bcad08941851dad
joemuromec <joemuromec@gmail.com>
2026-03-17 13:49:41 +0500

    added write files code for practice 6

---
+++ Practice6/file_handling/write_files.py
@@ -0,0 +1,5 @@
+with open("sample_data.txt", "w") as f:
+    f.write("Hello! Welcome to sample_data.txt\n")
+    f.write("This file is for testing purposes.\n")
+    f.write("Good Luck!\n")
+print("File has been created and content was added")
